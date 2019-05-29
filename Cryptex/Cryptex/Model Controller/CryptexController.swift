0234b2da5cae2fad79fabf47134dceb048855790
Enayatullah Naseri <ena.nas@icloud.com>
2019-05-29 12:18:29 -0700

    update added

--- Cryptex/Cryptex/Model Controller/CryptexController.swift
+++ Cryptex/Cryptex/Model Controller/CryptexController.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+
+class CryptexController {
+    
+    private(set) var cryptex: [Cryptex] = [Cryptex(password: "PASSWORD", hint: "Password is first of the Hint"), Cryptex(password: "OREGON", hint: "The state I am currently in"), Cryptex(password: "SUN", hint: "The star we can see every day")]
+    
+    init () {
+        randomCryptex()
+        
+    }
+    
+    
+    var currentCryptex: Cryptex?
+    
+    func randomCryptex() {
+        currentCryptex = cryptex.randomElement()
+    }
+    
+    
+}
--- Cryptex/Cryptex/View Controllers/CryptexViewController.swift
+++ Cryptex/Cryptex/View Controllers/CryptexViewController.swift
@@ -10,15 +10,29 @@ import UIKit
 
 class CryptexViewController: UIViewController {
     
+    var cryptexController = CryptexController()
+    
     @IBOutlet weak var hintLabel: UILabel!
     @IBOutlet weak var cryptexPickerView: UIPickerView!
     @IBOutlet weak var unlockButton: UIButton!
     
+    var letters = ["A", "B", "C", "D",
+                   "E", "F", "G", "H",
+                   "I", "J", "K", "L",
+                   "M", "N", "O", "P",
+                   "Q", "R", "S", "T",
+                   "U", "V", "W", "X",
+                   "Y", "Z"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateViews()
 
-        // Do any additional setup after loading the view.
+    }
+    
+    func updateViews () {
+        guard let hint = cryptexController.currentCryptex?.hint else { return }
+        hintLabel.text = hint // set hind label
     }
     
     @IBAction func unlockButtonTapped(_ sender: Any) {
@@ -26,4 +40,25 @@ class CryptexViewController: UIViewController {
     }
     
 
+}
+
+extension CryptexViewController: UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        // For the number of components, think about how you can figure out how many characters are in the `currentCryptex`'s password.
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        // For the number of rows, we want to show as many rows as there are letters.
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        // For the title of each row, we want to show the letter that corresponds to the row. i.e. row 0 should show "A", row 1 should show "B", etc.
+    }
+    
+}
+
+extension CryptexViewController: UIPickerViewDelegate {
+    
+    
 }
